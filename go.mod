2060ff00bd694722534c411bb4de2910d9c7e316
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-01-23 18:18:59 +0700

    feat: Add authentication logic

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/Namchee/free-weekend
 go 1.15
 
 require (
-	github.com/google/go-github/v33 v33.0.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20210113205817-d3ed898aa8a3 // indirect
+	github.com/google/go-github/v33 v33.0.0
+	golang.org/x/oauth2 v0.0.0-20210113205817-d3ed898aa8a3
 )
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
 	"strconv"
+	"strings"
+
+	"github.com/google/go-github/v33/github"
+	"golang.org/x/oauth2"
 )
 
 const (
@@ -15,8 +21,18 @@ type Configuration struct {
 	message string
 }
 
+type Repository struct {
+	author string
+	name   string
+}
+
 func getConfiguration() Configuration {
 	token := os.Getenv("INPUT_ACCESS_TOKEN")
+
+	if len(token) == 0 {
+		log.Fatalln("GitHub access token is required")
+	}
+
 	offset, _ := strconv.Atoi(os.Getenv("INPUT_TIMEZONE_OFFSET"))
 	message := os.Getenv("INPUT_MESSAGE")
 
@@ -27,6 +43,33 @@ func getConfiguration() Configuration {
 	}
 }
 
+func getRepository(metadata string) *Repository {
+	tokens := strings.Split(metadata, "/")
+
+	if len(tokens) != 2 {
+		return nil
+	}
+
+	return &Repository{
+		author: tokens[0],
+		name:   tokens[1],
+	}
+}
+
 func main() {
 	config := getConfiguration()
+
+	ctx := context.Background()
+
+	token_source := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: config.token},
+	)
+	oauth_client := oauth2.NewClient(ctx, token_source)
+	client := github.NewClient(oauth_client)
+
+	repository := getRepository(os.Getenv("GITHUB_REPOSITORY"))
+
+	if repository == nil {
+		log.Fatalln("Failed to read repository metadata")
+	}
 }
