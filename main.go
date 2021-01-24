a47606df531e9fc8ca694439d963d574e3480239
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-01-24 19:47:04 +0700

    feat: Add weekend logic checks

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
-	"strconv"
 	"strings"
+	"time"
 
 	"github.com/google/go-github/v33/github"
 	"golang.org/x/oauth2"
@@ -15,30 +16,41 @@ const (
 	closed = "closed"
 )
 
+type Event struct {
+	Action string "json:action"
+	Number int    "json:number"
+}
+
 type Configuration struct {
-	token   string
-	offset  int8
-	message string
+	token    string
+	location *time.Location
+	message  string
 }
 
 type Repository struct {
-	author string
-	name   string
+	owner string
+	name  string
 }
 
-func getConfiguration() Configuration {
+func getConfiguration() *Configuration {
 	token := os.Getenv("INPUT_ACCESS_TOKEN")
 
 	if len(token) == 0 {
 		log.Fatalln("GitHub access token is required")
 	}
 
-	offset, _ := strconv.Atoi(os.Getenv("INPUT_TIMEZONE_OFFSET"))
+	timezoneLocation := os.Getenv("INPUT_TIMEZONE")
+	location, err := time.LoadLocation(timezoneLocation)
+
+	if err != nil {
+		log.Fatalln("Ilegal timezone location name")
+	}
+
 	message := os.Getenv("INPUT_MESSAGE")
 
-	return Configuration{
+	return &Configuration{
 		token,
-		int8(offset),
+		location,
 		message,
 	}
 }
@@ -51,8 +63,8 @@ func getRepository(metadata string) *Repository {
 	}
 
 	return &Repository{
-		author: tokens[0],
-		name:   tokens[1],
+		owner: tokens[0],
+		name:  tokens[1],
 	}
 }
 
@@ -64,12 +76,102 @@ func main() {
 	token_source := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: config.token},
 	)
-	oauth_client := oauth2.NewClient(ctx, token_source)
-	client := github.NewClient(oauth_client)
+	oauthClient := oauth2.NewClient(ctx, token_source)
+	client := github.NewClient(oauthClient)
 
 	repository := getRepository(os.Getenv("GITHUB_REPOSITORY"))
 
 	if repository == nil {
 		log.Fatalln("Failed to read repository metadata")
 	}
+
+	var event Event
+	event_file, err := os.Open(os.Getenv("GITHUB_EVENT_PATH"))
+
+	if err != nil {
+		log.Fatalln("Failed to read event metadata")
+	}
+
+	if err = json.NewDecoder(event_file).Decode(&event); err != nil {
+		log.Fatalln("Failed to parse event metadata")
+	}
+
+	pullRequest, _, err := client.PullRequests.Get(
+		ctx,
+		repository.owner,
+		repository.name,
+		event.Number,
+	)
+
+	if err != nil {
+		log.Fatalln("Failed to fetch pull request information")
+	}
+
+	day := pullRequest.GetCreatedAt().In(config.location).Weekday()
+
+	if day == 7 || day == 0 { // if PR is submitted on weekends, close it
+		err = closePullRequest(
+			&ctx,
+			client,
+			config.message,
+			repository.name,
+			repository.owner,
+			pullRequest.GetNumber(),
+		)
+
+		if err != nil {
+			log.Fatalln("Failed to close pull request due to unexpected errors")
+		}
+	}
+}
+
+func closePullRequest(
+	ctx *context.Context,
+	client *github.Client,
+	reason string,
+	repository string,
+	owner string,
+	number int,
+) error {
+	_, _, err := client.Issues.CreateComment(
+		*ctx,
+		owner,
+		repository,
+		number,
+		&github.IssueComment{
+			Body: github.String(reason),
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	_, _, err = client.Issues.AddLabelsToIssue(
+		*ctx,
+		owner,
+		repository,
+		number,
+		[]string{"good-weekend"},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	_, _, err = client.PullRequests.Edit(
+		*ctx,
+		owner,
+		repository,
+		number,
+		&github.PullRequest{
+			State: github.String(closed),
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
